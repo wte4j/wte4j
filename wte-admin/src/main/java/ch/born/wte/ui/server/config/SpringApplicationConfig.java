a8f7a8710a563d880dd1ffe0e2766c63e023e18b
oliver Freivogel <oliver.freivogel@gmail.com>
2015-03-02 11:41:22 +0100

    wte-admin switch to spring java config

---
+++ wte-admin/src/main/java/ch/born/wte/ui/server/config/SpringApplicationConfig.java
@@ -0,0 +1,24 @@
+package ch.born.wte.ui.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+@ComponentScan(basePackages={"ch.born.wte.ui.server.services", "ch.born.wte.impl"})
+@PropertySource("/WEB-INF/wte.properties")
+@ImportResource("classpath:datasource-config.xml")
+public class SpringApplicationConfig {
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
