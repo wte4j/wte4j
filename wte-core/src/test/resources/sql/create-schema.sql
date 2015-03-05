3f3a5074ca9b716cf13cc0a578a85aede6c39887
Oliver Freivogel <Oliver@Freivogel.born.ch>
2015-03-05 17:24:35 +0100

    wte-core: generate schema for test with openJPA maven plugin

--- wte-core/src/test/resources/sql/create-schema.sql
+++ wte-core/src/test/resources/sql/create-schema.sql
@@ -1,4 +1,4 @@
-create table wte_template (id bigint not null, content longvarbinary, created_at timestamp not null, document_name varchar(255) not null, edited_at timestamp not null, editor_display_name varchar(100), editor_user_id varchar(50) not null, input_class_name varchar(250), language varchar(255) not null, locking_date timestamp, locking_user_display_name varchar(100), locking_user_id varchar(50), version bigint, primary key (id), unique (document_name, language));
-create table wte_template_properties (template_id bigint not null, property_value varchar(255), property_key varchar(255) not null, primary key (template_id, property_key));
-alter table wte_template_properties add constraint FKC9B09C81E7119E5D foreign key (template_id) references wte_template;
-create table wte_gen ( sequence_name varchar(255),  sequence_next integer ) ;
+CREATE TABLE wte_gen (SEQUENCE_NAME VARCHAR(255) NOT NULL, SEQUENCE_NEXT BIGINT, PRIMARY KEY (SEQUENCE_NAME));
+CREATE TABLE wte_template (id BIGINT NOT NULL, content BLOB, created_at TIMESTAMP NOT NULL, document_name VARCHAR(255) NOT NULL, edited_at TIMESTAMP NOT NULL, input_class_name VARCHAR(250), language VARCHAR(255) NOT NULL, locking_date TIMESTAMP, version BIGINT, editor_display_name VARCHAR(100), editor_user_id VARCHAR(50) NOT NULL, locking_user_display_name VARCHAR(100), locking_user_id VARCHAR(50), PRIMARY KEY (id), CONSTRAINT U_WT_TPLT_DOCUMENT_NAME UNIQUE (document_name, language));
+CREATE TABLE wte_template_properties (template_id BIGINT, property_key VARCHAR(255) NOT NULL, property_value VARCHAR(255));
+CREATE INDEX I_WT_TRTS_TEMPLATE_ID ON wte_template_properties (template_id);
