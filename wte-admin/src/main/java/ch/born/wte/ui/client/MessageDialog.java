0c9ca6e5656b8717ae6daefb8b975ea47b4cf8af
Oliver Freivogel <Oliver@Freivogel.born.ch>
2015-03-09 14:45:37 +0100

    table layout and icons

--- wte-admin/src/main/java/ch/born/wte/ui/client/MessageDialog.java
+++ wte-admin/src/main/java/ch/born/wte/ui/client/MessageDialog.java
@@ -10,112 +10,110 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-
 public class MessageDialog extends DialogBox {
 
-    public static final int INFO = 0;
-    public static final int WARN = 1;
-    public static final int ERROR = 2;
-    public static final int QUESTION = 3;
+	public static final int INFO = 0;
+	public static final int WARN = 1;
+	public static final int ERROR = 2;
+	public static final int QUESTION = 3;
 
-    private ClickHandler okHandler;
+	private ClickHandler okHandler;
 
+	public MessageDialog(String titel, String msg, int type, int width) {
+		init(titel, msg, type, width);
+	}
 
-    public MessageDialog(String titel, String msg, int type, int width) {
-        init(titel, msg, type, width);
-    }
+	public MessageDialog(String titel, String msg, int type) {
+		init(titel, msg, type, 200);
+	}
 
-    public MessageDialog(String titel, String msg, int type) {
-        init(titel, msg, type, 200);
-    }
+	public MessageDialog(String titel, String msg, int type, ClickHandler okHandler) {
+		// this.type=type;
+		this.okHandler = okHandler;
+		init(titel, msg, type, 200);
+	}
 
-    public MessageDialog(String titel, String msg, int type, ClickHandler okHandler) {
-        //this.type=type;
-        this.okHandler = okHandler;
-        init(titel, msg, type, 200);
-    }
+	private void init(String titel, String msg, int type, int width) {
+		setText(titel);
 
-    private void init(String titel, String msg, int type, int width) {
-        setText(titel);
+		// DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
+		HorizontalPanel hPanel = new HorizontalPanel();
+		hPanel.setSpacing(10);
 
-        //DockLayoutPanel dock = new DockLayoutPanel(Unit.PX);
-        HorizontalPanel hPanel = new HorizontalPanel();
-        hPanel.setSpacing(10);
+		final Image img;
+		switch (type) {
+		case WARN:
+			img = new Image(Application.RESOURCES.warningImage());
+			break;
 
-        final Image img;
-        switch (type) {
-            case WARN:
-                img = new Image("images/warn.png");
-                break;
+		case ERROR:
+			img = new Image(Application.RESOURCES.errorImage());
+			break;
 
-            case ERROR:
-                img = new Image("images/error.png");
-                break;
+		case QUESTION:
+			img = new Image(Application.RESOURCES.questionImage());
+			break;
 
-            case QUESTION:
-                img = new Image("images/question.png");
-                break;
+		default:
+			img = new Image(Application.RESOURCES.informationImage());
+			break;
+		}
+		hPanel.add(img);
 
-            default:
-                img = new Image("images/info.png");
-                break;
-        }
-        hPanel.add(img);
+		VerticalPanel vPanel = new VerticalPanel();
+		vPanel.setSpacing(10);
+		// dock.add(panel);
 
-        VerticalPanel vPanel = new VerticalPanel();
-        vPanel.setSpacing(10);
-        //dock.add(panel);
+		Label msgLabel = new Label(msg);
+		vPanel.add(msgLabel);
 
-        Label msgLabel = new Label(msg);
-        vPanel.add(msgLabel);
+		if (type == QUESTION) {
+			Grid grid = new Grid(1, 2);
+			grid.setCellPadding(10);
+			vPanel.add(grid);
+			Button okButton = new Button("OK", new ClickHandler() {
 
-        if (type == QUESTION) {
-            Grid grid = new Grid(1, 2);
-            grid.setCellPadding(10);
-            vPanel.add(grid);
-            Button okButton = new Button("OK", new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					hide();
+					if (okHandler != null) {
+						okHandler.onClick(event);
+					}
 
-                @Override
-                public void onClick(ClickEvent event) {
-                    hide();
-                    if (okHandler != null) {
-                        okHandler.onClick(event);
-                    }
+				}
 
-                }
+			});
+			Button cancelButton = new Button("Cancel", new ClickHandler() {
 
-            });
-            Button cancelButton = new Button("Cancel", new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					hide();
 
-                @Override
-                public void onClick(ClickEvent event) {
-                    hide();
+				}
 
-                }
+			});
+			grid.setWidget(0, 0, okButton);
+			grid.setWidget(0, 1, cancelButton);
+		}
+		else {
+			Button closeButton = new Button("Close", new ClickHandler() {
 
-            });
-            grid.setWidget(0, 0, okButton);
-            grid.setWidget(0, 1, cancelButton);
-        }
-        else {
-            Button closeButton = new Button("Close", new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					hide();
 
-                @Override
-                public void onClick(ClickEvent event) {
-                    hide();
+				}
 
-                }
+			});
+			vPanel.add(closeButton);
+		}
 
-            });
-            vPanel.add(closeButton);
-        }
-
-        //dock.setWidth("100%");
-        vPanel.setWidth(width + "px");
-        hPanel.add(vPanel);
-        setWidget(hPanel);
-        setGlassEnabled(true);
-        center();
-    }
+		// dock.setWidth("100%");
+		vPanel.setWidth(width + "px");
+		hPanel.add(vPanel);
+		setWidget(hPanel);
+		setGlassEnabled(true);
+		center();
+	}
 
 }
--- wte-admin/src/main/java/ch/born/wte/ui/client/WteResources.java
+++ wte-admin/src/main/java/ch/born/wte/ui/client/WteResources.java
@@ -2,11 +2,26 @@ package ch.born.wte.ui.client;
 
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
 
 public interface WteResources extends ClientBundle {
-	@Source("images/context.png")
-	ImageResource contextMenuIcon();
-	
+
 	@Source("images/load.gif")
 	ImageResource loading();
+
+	@Source("images/toggle-down-alt.png")
+	@ImageOptions(height = 32, width = 32)
+	ImageResource contextMenuIcon();
+
+	@Source("images/exclamation.png")
+	ImageResource warningImage();
+
+	@Source("images/exclamation-circle.png")
+	ImageResource errorImage();
+
+	@Source("images/question-balloon.png")
+	ImageResource questionImage();
+
+	@Source("images/information.png")
+	ImageResource informationImage();
 }
