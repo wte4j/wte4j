129861adafde8bd753770dd15013ce21837f22a4
Thomas Selber <thomas.selber@born.ch>
2015-03-09 09:13:58 +0100

    remove unnecessary import in MainPanel
    There was an unnecessary import in the MainPanel.

--- wte-admin/src/main/java/ch/born/wte/ui/client/MainPanel.java
+++ wte-admin/src/main/java/ch/born/wte/ui/client/MainPanel.java
@@ -5,7 +5,6 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class MainPanel extends Composite {
