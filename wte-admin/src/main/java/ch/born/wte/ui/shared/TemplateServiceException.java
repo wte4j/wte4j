1978ba3a9cc4b25d93cbea1753ae2c55d11f9d6e
Oliver Freivogel <Oliver@Freivogel.born.ch>
2015-03-11 18:07:52 +0100

    fix gwt compile error

--- wte-admin/src/main/java/ch/born/wte/ui/shared/TemplateServiceException.java
+++ wte-admin/src/main/java/ch/born/wte/ui/shared/TemplateServiceException.java
@@ -1,6 +1,10 @@
 package ch.born.wte.ui.shared;
 
+
 public class TemplateServiceException extends RuntimeException {
+
+	public TemplateServiceException() {
+	}
 
 	public TemplateServiceException(String message) {
 		super(message);
