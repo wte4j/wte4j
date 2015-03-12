8a93eba4c7ac869b3f072fc2f3745215206cac02
Oliver Freivogel <Oliver@Freivogel.born.ch>
2015-03-12 14:22:32 +0100

    Read Locale for message generation from request

--- wte-admin/src/main/java/ch/born/wte/ui/server/filters/SessionContextFilter.java
+++ wte-admin/src/main/java/ch/born/wte/ui/server/filters/SessionContextFilter.java
@@ -18,31 +18,37 @@ import ch.born.wte.ui.server.services.ServiceContext;
 public class SessionContextFilter implements Filter {
 
 	private ServiceContext serviceContext;
-	
+
 	@Override
 	public void destroy() {
-		
+
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain filterChain) throws IOException, ServletException {
-		String userName = null;
-		if (request instanceof HttpServletRequest && ((HttpServletRequest)request).getUserPrincipal() != null) {
-			userName = ((HttpServletRequest)request).getUserPrincipal().getName();
+
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+			if (httpRequest.getUserPrincipal() != null) {
+				String userName = httpRequest.getUserPrincipal().getName();
+				if (userName != null) {
+					serviceContext.setUser(new User(userName, userName));
+				}
+				serviceContext.setLocale(httpRequest.getLocale());
+			}
+
 		}
-		if (userName != null) {
-			serviceContext.setUser(new User(userName,userName));
-		}
-		if (filterChain != null)
-			filterChain.doFilter(request, response);
+
+		filterChain.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		serviceContext = WebApplicationContextUtils.
-				  getRequiredWebApplicationContext(filterConfig.getServletContext()).
-				  getBean(ServiceContext.class);
+				getRequiredWebApplicationContext(filterConfig.getServletContext()).
+				getBean(ServiceContext.class);
 	}
 
 }
--- wte-admin/src/main/java/ch/born/wte/ui/server/services/ServiceContext.java
+++ wte-admin/src/main/java/ch/born/wte/ui/server/services/ServiceContext.java
@@ -4,11 +4,12 @@ import java.util.Locale;
 
 import ch.born.wte.User;
 
-
 public interface ServiceContext {
 	User getUser();
 
 	void setUser(User user);
 
 	Locale getLocale();
+
+	void setLocale(Locale locale);
 }
