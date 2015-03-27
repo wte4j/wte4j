f3a84aab39d3b5adfca1378ac453b5d32a6fb413
Oliver Freivogel <Oliver@Freivogel.born.ch>
2015-03-27 16:17:30 +0100

    fix spring configuration

--- wte4j-admin-war/src/main/java/org/wte4j/ui/config/RootApplicationConfig.java
+++ wte4j-admin-war/src/main/java/org/wte4j/ui/config/RootApplicationConfig.java
@@ -2,15 +2,18 @@ package org.wte4j.ui.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.wte4j.ui.server.config.Wte4jAdminConfig;
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("WEB-INF/wte4j.properties")
 @ImportResource({ "WEB-INF/wte4j-model-service-config.xml", "classpath:wte4j-core-application-context.xml" })
+@Import({ StandaloneJPAConfig.class, Wte4jAdminConfig.class })
 public class RootApplicationConfig {
 
 	@Bean
