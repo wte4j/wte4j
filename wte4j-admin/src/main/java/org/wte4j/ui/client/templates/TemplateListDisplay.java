f267b7916f93125929995c78f713febc7a096c6d
Oliver Freivogel <oliver.freivogel@born.ch>
2015-04-20 16:47:08 +0200

    use bootstrap in context menu

--- wte4j-admin/src/main/java/org/wte4j/ui/client/templates/TemplateListDisplay.java
+++ wte4j-admin/src/main/java/org/wte4j/ui/client/templates/TemplateListDisplay.java
@@ -17,7 +17,7 @@ package org.wte4j.ui.client.templates;
 
 import org.wte4j.ui.shared.TemplateDto;
 
-import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.view.client.HasData;
 
@@ -25,18 +25,18 @@ public interface TemplateListDisplay extends IsWidget {
 
 	HasData<TemplateDto> getDataContainer();
 
-	void setDowndLoadCommand(ScheduledCommand command);
+	void setDowndLoadCommand(ClickHandler command);
 
-	void setUpdateCommand(ScheduledCommand command);
+	void setUpdateCommand(ClickHandler command);
 
 	void setUnLockCommandVisible(boolean visible);
 
-	void setUnlockCommand(ScheduledCommand command);
+	void setUnlockCommand(ClickHandler command);
 
 	void setLockCommandVisible(boolean visible);
 
-	void setLockCommand(ScheduledCommand command);
+	void setLockCommand(ClickHandler command);
 
-	void setDeleteCommand(ScheduledCommand command);
+	void setDeleteCommand(ClickHandler command);
 
 }
